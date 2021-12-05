a7d3e9bf22fcc297ea75b28d487bc68053514deb
rajmahal4 <rsingh6@mail.sfsu.edu>
2021-12-04 23:53:52 -0800

    should fix images

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import { headline } from "./modules/headline";
 import { footer } from "./modules/footer";
 import { menu } from "./modules/menu";
 import { about } from "./modules/about";
+import data from '../src/data/data.json'
+import al from './assets/al.jpg'
+import bundy from './assets/bundy.png'
+import gacy from './assets/gacy.png'
+import jeff from './assets/jeff.jpg'
+import karla from './assets/karla.png'
+import tim from './assets/tim.png'
+
+
+
 const  loadPage = (()=> {
     navbar();
     headline();
