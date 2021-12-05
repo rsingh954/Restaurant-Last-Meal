6c2199a53d9782201f26268898a3402d6c8e6611
rajmahal4 <rsingh6@mail.sfsu.edu>
2021-12-04 22:54:31 -0800

    fixed stuff again AND added pics

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,35 +1,7 @@
 import data from '../data/data.json' 
 
-
-const createMenuItem = (data) => {
-    const content = document.querySelector('#content')
-    const div = document.createElement('div')
-    const card  = document.createElement('card')
-    const img = document.createElement('img')
-    const container = document.createElement('div')
-    div.classList.add('menu')
-    div.innerHTML = `
-    <div class="card">
-    <img src="${data[0].src}" alt="Meal" style = 'height: 20vh'>
-    <div class="menu-container">
-      <h4><b>The ${data[0].name}</b></h4>
-      <p>${data[0].meal}</p>
-      <div>
-        <button class = "dropdown">Additional Info </button>
-        <div class = "dropdown-content">${data[0].description}</div>
-    <div>
-    </div>
-  </div> 
-    `
-
-    content.appendChild(div)
-}
-
 const menu = () =>{
     const content = document.querySelector('#content')
-
-    const card  = document.createElement('card')
-    const img = document.createElement('img')
     const container = document.createElement('div')
     container.classList.add('menu-container-flex')
     container.style.display = 'none'
@@ -38,6 +10,7 @@ const menu = () =>{
         div.classList.add('menu')
         div.innerHTML = `
         <div class="card">
+        <img src="${data[i].src}" alt="Meal" style = 'height: 100px; width: 100px;'>
         <div class="menu-container">
           <h4><b>The ${data[i].name}</b></h4>
           <p>${data[i].meal}</p>
@@ -50,11 +23,7 @@ const menu = () =>{
         `
         container.appendChild(div)
     }
-    content.appendChild(container)
-    // card.appendChild(img)
-    // card.appendChild(container)
-    // div.appendChild(card)
-  
+    content.appendChild(container)  
 }
 
 export {menu}
