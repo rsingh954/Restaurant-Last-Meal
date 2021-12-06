e4a38d7e314e38ea53a6e4373d794fe62c163722
rajmahal4 <rsingh6@mail.sfsu.edu>
2021-12-06 09:44:23 -0800

    fixed mobile positioning v1

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -3,19 +3,17 @@ import '../style.css'
 
 const navbar = () =>{
     const header = document.createElement('header');
-    const nav = document.createElement('nav')
     const content = document.querySelector('#content')
 
 
-    nav.innerHTML = `
+    header.innerHTML = `
     <ul class = 'nav_links'>
         <li><a href ="#">Last Meals</a></li>
         <li><a href ="#">Menu</a></li>
         <li><a href ="#">About</a></li>
     </ul>
     `
-    
-    header.appendChild(nav)
+
     content.appendChild(header)
 }
 
